9e7cbaf4fc38b42d3bd99b94af1eb28104837797
Shone Binu <shonebinualias@gmail.com>
2022-11-30 21:44:48 +0530

    comments for leap year

--- firstPrograms/leapYear.c
+++ firstPrograms/leapYear.c
@@ -5,13 +5,25 @@ int main() {
     printf("Enter an year: ");
     scanf("%d", &year);
 
+   // leap year if perfectly divisible by 400
     if (year % 400 == 0) {
         printf("%d is a leap year.", year);
-    } else if (year % 100 == 0) {
+    } 
+
+   // not a leap year if divisible by 100
+   // but not divisible by 400
+    else if (year % 100 == 0) {
         printf("%d is not a leap year.", year);
-    } else if (year % 4 == 0) {
+    } 
+    
+   // leap year if not divisible by 100
+   // but divisible by 4
+    else if (year % 4 == 0) {
         printf("%d is a leap year.", year);
-    } else {
+    } 
+    
+   // all other years are not leap years
+    else {
         printf("%d is not a leap year.", year);
     }
 
