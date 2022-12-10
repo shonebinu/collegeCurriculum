c05c353f70589b97cdd2bcc19c34e0f15111d129
Shone Binu <shonebinualias@gmail.com>
2022-12-10 17:21:08 +0530

    s

--- variables.c
+++ variables.c
@@ -7,6 +7,8 @@ int main() {
 
     printf("There once was a man named %s.\n", characterName);
     printf("he was %d years old. \n", characterAge);
+
+    characterAge = 30;
     printf("He really liked the name %s.\n", characterName);
     printf("but did not like being %d.\n", characterAge);
     
