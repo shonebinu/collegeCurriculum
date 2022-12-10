e8636c315193ceea5caeec6609c5ebd3546277eb
Shone Binu <shonebinualias@gmail.com>
2022-12-10 17:19:09 +0530

    variables complete

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    char characterName[] = "Shone Binu";
+    int characterAge = 35;
+
+    printf("There once was a man named %s.\n", characterName);
+    printf("he was %d years old. \n", characterAge);
+    printf("He really liked the name %s.\n", characterName);
+    printf("but did not like being %d.\n", characterAge);
+    
+    return 0;
+}
