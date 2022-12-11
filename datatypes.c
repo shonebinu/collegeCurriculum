1705058b113ad89f3959620e15020e9313ccb3ba
Shone Binu <shonebinualias@gmail.com>
2022-12-11 08:00:51 +0530

    printf

---
+++ datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 40;
+    float gpa = 3.7;
+    char grade = 'A';
+    char phrase[] = "Shone is the best.";
+
+    return 0;
+}
---
+++ printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello\nWorld\n");
+    printf("Hello\"World\n");
+
+    printf("My favourite %s is %f.\n", "number", 500.4342);
+
+    int favNum = 90;
+    printf("My favourite number is %d.\n", favNum);
+
+    return 0;
+}
