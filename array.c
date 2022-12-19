3dd76b4c15033cc97826f8f3ad6f91659ef206f1
Shone Binu <shonebinualias@gmail.com>
2022-12-19 10:04:25 +0530

    array

---
+++ array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int lukcyNumbers[] = {4, 8, 15, 16, 23, 42};
+  char phrase[] = "Shone Binu"; // This is an array
+
+  printf("%d", lukcyNumbers[1]);
+
+  lukcyNumbers[3] = 200;
+
+  printf("%d", lukcyNumbers[3]);
+
+  return 0;
+}
