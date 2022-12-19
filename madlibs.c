5b3a5dafa4f2a7ee32202d37f155e966fb543d16
Shone Binu <shonebinualias@gmail.com>
2022-12-19 09:49:20 +0530

    madlibs

---
+++ madlibs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+  char color[20], pluralNoun[20], celebrityF[20], celebrityL[20];
+
+  printf("Enter a color: ");
+  scanf("%s", color);
+  printf("Enter a plural noun: ");
+  scanf("%s", pluralNoun);
+  printf("Enter a celebrity: ");
+  scanf("%s%s", celebrityF, celebrityL);
+
+  printf("Roses are %s\n", color);
+  printf("%s are blue\n", pluralNoun);
+  printf("I love %s %s\n", celebrityF, celebrityL);
+}
