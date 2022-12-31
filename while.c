3a6d3380f2f714e3893706781312e10c09a31eeb
Shone Binu <shonebinualias@gmail.com>
2022-12-31 19:05:38 +0530

    do while loop

--- while.c
+++ while.c
@@ -10,5 +10,19 @@ int main()
     index++;
   }
 
+  index = 6;
+  while (index <= 5) // since it checks the condition before running the loop, nothing is executed
+  {
+    printf("Shone %d\n", index);
+    index++;
+  }
+
+  // index = 6
+  do
+  {
+    printf("Shone %d\n", index);
+    index++;
+  } while (index <= 5); // here, 6 print's because the condition is checked only after the loop ran once
+
   return 0;
 }
