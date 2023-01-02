97148f98cd085d3a6b5e4356a3fdb423555a6e8e
Shone Binu <shonebinualias@gmail.com>
2023-01-02 15:10:57 +0530

    guessing game

---
+++ guessingGame.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int secretNumber = 5;
+  int guess, guessCount = 0;
+  int guessLimit = 3;
+  int outOfGuesses = 0;
+
+  while (guess != secretNumber && outOfGuesses == 0)
+  {
+    if (guessCount < guessLimit)
+    {
+      printf("Enter a number: ");
+      scanf("%d", &guess);
+      guessCount++;
+    }
+    else
+    {
+      outOfGuesses = 1;
+    }
+  }
+  if (outOfGuesses == 1)
+    printf("Out of guesses\n");
+  else
+    printf("You Win!\n");
+
+  return 0;
+}
