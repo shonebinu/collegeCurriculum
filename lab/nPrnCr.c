66f8a243d1aaf1995b6637a3cb76e5bf4737776d
Shone Binu <shonebinualias@gmail.com>
2023-02-02 22:29:14 +0530

    nPrnCr

--- lab/nPrnCr.c
+++ lab/nPrnCr.c
@@ -5,11 +5,15 @@ int rec(int num);
 void main()
 {
   int n, r, nPr, nCr;
-  printf("Enter the values of 'n' and 'r': ");
+  printf("Enter the values of 'n' and 'r': \n");
   printf("n : ");
   scanf("%d", &n);
   printf("r : ");
   scanf("%d", &r);
+  nPr = rec(n) / (rec(n - r));
+  nCr = nPr / rec(r);
+  printf("nPr = %d\n", nPr);
+  printf("nCr = %d", nCr);
 }
 
 int rec(int num)
