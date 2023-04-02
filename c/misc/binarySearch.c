e052dda1c037bdbc8407bf1ec7e11658ae736074
Shone Binu <shonebinualias@gmail.com>
2023-04-02 16:41:52 +0530

    binary search

---
+++ c/misc/binarySearch.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int binarySearch(int x[], int size, int key) {
+    int L, R, mid;
+    
+    while ()
+}
+
+int main() {
+    int array[5] = {1, 4, 2, 5, 9};
+    printf("Location: %d\n", linearSearch(array, 5, 5));
+    return 0;
+}
