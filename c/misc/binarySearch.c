28da23d5d454e0c51983c75785d10c98468a04f5
Shone Binu <shonebinualias@gmail.com>
2023-04-02 17:12:34 +0530

    binary search

--- c/misc/binarySearch.c
+++ c/misc/binarySearch.c
@@ -2,12 +2,19 @@
 
 int binarySearch(int x[], int size, int key) {
     int L, R, mid;
-    
-    while ()
+    L = -1; R = size;
+    while (L + 1 != R) {
+        mid = (L+R) / 2;
+        if (x[mid] > key)
+            R = mid;
+        else L = mid;
+    }
+    if (L >= 0 && x[L] == key) return L;
+    else return -1;
 }
 
 int main() {
     int array[5] = {1, 4, 2, 5, 9};
-    printf("Location: %d\n", linearSearch(array, 5, 5));
+    printf("Location: %d\n", binarySearch(array, 5, 5));
     return 0;
 }
