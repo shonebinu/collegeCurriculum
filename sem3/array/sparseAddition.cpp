ff094cc8b288a35b0fd1385e647b41510d9ab820
Shone Binu <shonebinualias@gmail.com>
2023-07-21 22:50:53 +0530

    spadd

---
+++ sem3/array/sparseAddition.cpp
@@ -0,0 +1,14 @@
+// done myself entirely
+
+#include <iostream>
+
+using namespace std;
+
+class Array {
+  public:
+    int array[10][10];
+    void create();
+    void traverse();
+    void toTriplet();
+    void tripletAdd(Array);
+};
