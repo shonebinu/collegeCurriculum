89076ea3b926d4dd9094ff7f1425c3dcf30f85b4
Shone Binu <shonebinualias@gmail.com>
2023-08-20 15:18:38 +0530

    bubble sort add

--- sem3/array/bubbleSort.cpp
+++ sem3/array/bubbleSort.cpp
@@ -4,22 +4,41 @@ using namespace std;
 
 void swap(int&, int&);
 
+class Array {
+  public:
+    int length, array[20];
+
+    void read();
+    void bubbleSort();
+    void display();
+};
+
 int main() {
-
-  int length = 5;
-  int list[] = { 100, 20, 80, 60, 40 };
-
-  // Bubble sort
-  for (int i = length - 1; i > 0; i--) 
-    for (int j = 0; j < i; j++)
-      if (list[j] > list[j + 1]) 
-        swap(list[j], list[j + 1]);
-  // END  
-
-  for (int i = 0; i < length; i++)
-    cout << list[i] << '\t';
-  
+  Array object;
+  object.read();
+  object.bubbleSort();
+  object.display();
   return 0;
+}
+
+void Array::read() {
+  cout << "Enter the number of elements: ";
+  cin >> length;
+  cout << "Enter the elements: ";
+  for (int i = 0; i < length; i++)
+    cin >> array[i];
+}
+
+void Array::bubbleSort() {
+  for (int i = length - 1; i > 0; i--)
+    for (int j = 0; j < i; j++)
+      if (array[j] > array[j + 1])
+        swap(array[j], array[j + 1]);
+}
+
+void Array::display() {
+  for (int i  = 0; i < length; i++)
+    cout << array[i] << '\t';
 }
 
 void swap(int& a, int& b) {
