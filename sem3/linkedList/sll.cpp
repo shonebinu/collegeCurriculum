a4b0e0bd6f1326508eeb83cabd7ad8a30f8d0f35
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-01-17 13:12:27 +0530

    fix: change singly linked list

--- sem3/linkedList/sll.cpp
+++ sem3/linkedList/sll.cpp
@@ -3,190 +3,183 @@
 using namespace std;
 
 struct Node {
-    int item;
-    struct Node *next;
+  int item;
+  struct Node *next;
 };
 
 typedef struct Node node;
 
-class SL {
-    public:
-        int i, value;
-        node *start, *temp, *ptr;
-        void create();
-        void traversal();
-        void search();
-        void in_beg(); // insert at beginning
-        void in_end();
-        void in_spec(); // insert at a specific position
-        void del_beg();
-        void del_end();
-        void del_spec();
-        
-        SL() {
-            start = 0;
-        }
+class SLL {
+  public:
+    node *start, *temp, *ptr;
+    void create();
+    void traverse();
+    void search();
+    void insertBeg();
+    void insertEnd();
+    void insertPos();
+    void delBeg();
+    void delEnd();
+    void delPos();
+  SLL() {
+    start = 0;
+  }
 };
 
-void SL::in_beg() {
-    int value;
-    cout << endl << "Enter the item to be inserted: ";
-    cin >> value;
-    ptr = new node;
-    ptr -> item = value;
-    ptr -> next = 0;
-    temp -> next = ptr;
+void SLL::create() {
+  ptr = new node;
+  ptr -> next = 0;
+  cout << "Enter the item: ";
+  cin >> ptr -> item;
+  if (start == 0) {
+    start = ptr;
+    return;
+  }
+
+  temp = start;
+  while (temp -> next != 0) 
+    temp = temp -> next;
+  temp -> next = ptr;
 }
 
-void SL::in_end() {
-    int value;
-    temp = start;
-    while (temp -> next != 0) 
-        temp = temp -> next;
-    cout << "Enter the item to be inserted: ";
-    cin >> value;
-    ptr = new node;
-    ptr -> item = value;
-    ptr -> next = 0;
-    temp -> next = ptr;
+void SLL::traverse() {
+  if (start == 0) {
+    cout << "The linked list is empty" << endl;
+    return;
+  }
+
+  temp = start;
+  cout << "The elements are: ";
+  while (temp) {
+    cout << temp -> item << " ";
+    temp = temp -> next;
+  }
+  
+  cout << endl;
 }
 
-void SL::in_spec() {
-    int i, pos, value;
-    cout << "Enter the position: ";
-    cin >> pos;
-    if (pos == 1) 
-        in_beg();
-    else {
-        temp = start;
-        for (i = 1; i < pos-1; i++) {
-            temp = temp -> next;
-            if (temp == 0) {
-                cout << endl << "No such position exists";
-                break;
-            }
-        }
-        if (temp != 0) {
-            cout << endl << "Enter the item to be inserted: ";
-            cin >> value;
-            ptr = new node;
-            ptr -> item = value;
-            ptr -> next = temp -> next;
-            temp -> next = ptr;
-        }
+void SLL::search() {
+  int value, pos = 0;
+  cout << "Enter the item to be searched: ";
+  cin >> value;
+  temp = start;
+  while (temp) {
+    pos++;
+    if (temp -> item == value) {
+      cout << value << " is present in the list at the position " << pos << endl;
+      return;
     }
+    temp = temp -> next;
+  }
+  cout << "The item is not present in the list" << endl;
 }
 
-void SL::del_beg() {
-    temp = start;
-    start = start -> next;
-    cout << "Deleted item is: " << temp -> item;
-    delete(temp);
+void SLL::insertBeg() {
+  ptr = new node;
+  cout << "Enter the value: ";
+  cin >> ptr -> item;
+  ptr -> next = start;
+  start = ptr;
 }
 
-void SL::del_end() {
-    ptr = start;
-    while (ptr -> next -> next != 0) {
-        ptr = ptr -> next;
-    }
-    temp = ptr -> next;
-    ptr -> next = 0;
-    cout << "Deleted item is: " << temp -> item;
-    delete(temp);
+void SLL::insertEnd() {
+  ptr = new node;
+  // ptr -> next = 0;
+  cout << "Enter the value: ";
+  cin >> ptr -> item;
+
+  temp = start;
+  while (temp -> next) 
+    temp = temp -> next;
+
+  temp -> next = ptr;
 }
 
-void SL::del_spec() {
-    int pos, i;
-    cout << "Enter the position to be deleted: ";
-    cin >> pos;
-    if (pos == 1) 
-        del_beg();
-    else {
-        ptr = start;
-        for (i = 1; i < pos-1; i++) {
-            ptr = ptr -> next;
-            if (ptr == 0 || ptr -> next == 0) {
-                cout << "Invalid position";
-                return;
-            }
-        }
-        if (ptr != 0) {
-            temp = ptr -> next;
-            ptr -> next = temp -> next;
-            cout << "Deleted item is: " << temp -> item;
-            delete(temp);
-        }
+void SLL::insertPos() {
+  int pos;
+  cout << "Enter the position: ";
+  cin >> pos;
+  if (pos == 1) {
+    insertBeg();
+    return;
+  }
+  
+  temp = start;
+  for (int i = 1; i < pos-1; i++) {
+    temp = temp -> next;
+
+    if (!temp) {
+      cout << endl << "No such position exists";
+      return;
     }
+  }
+
+  ptr = new node;
+  cout << "Enter the value: ";
+  cin >> ptr -> item;
+  ptr -> next = temp -> next;
+  temp -> next = ptr;
 }
 
-void SL::create() {
-    cout << "Enter the item: ";
-    cin >> value;
-    ptr = new node;
-    ptr -> item = value;
-    ptr -> next = 0;
-    if (start == 0)
-        start = ptr;
-    else {
-        temp = start;
-        while (temp -> next != 0) 
-            temp = temp -> next;
-        temp -> next = ptr;
-    }
+void SLL::delBeg() {
+  temp = start;
+  start = start -> next;
+  cout << "Deleted item is: " << temp -> item;
+  delete temp;
 }
 
-void SL::traversal() {
-    if (start == 0) {
-        cout << endl << "The list is empty";
-        return;
-    }
-    ptr = start;
-    cout << "The list is: ";
-    while (ptr != 0) {
-        cout << ptr -> item << endl;
-        ptr = ptr -> next;
-    }
+void SLL::delEnd() {
+  ptr = start;
+  while (ptr -> next -> next) 
+    ptr = ptr -> next;
+  temp = ptr -> next;
+  ptr -> next = 0;
+  cout << "Deleted item is: " << temp -> item;
+  delete temp;
 }
 
-void SL::search() {
-    int item, flag = 0;
-    cout << "Enter the item to be searched: ";
-    cin >> item;
-    temp = start;
-    while (temp != 0) {
-        if (temp -> item == item) {
-            flag = 1;
-            break;
-        }
-        temp = temp -> next;
+void SLL::delPos() {
+  int pos;
+  cout << "Enter the position to be deleted: ";
+  cin >> pos;
+  if (pos == 1) {
+    delBeg();
+    return;
+  }
+
+  ptr = start;
+  for (int i = 1; i < pos-1; i++) {
+    ptr = ptr -> next;
+    if (!ptr -> next) {
+      cout << "Invalid position";
+      return;
     }
-    if (flag == 1)
-        cout << endl << "Item is present in the list.";
-    else 
-        cout << endl << "Item is not present";
+  }
+  temp = ptr -> next;
+  ptr -> next = temp -> next;
+  cout << "Deleted item is: " << temp -> item << endl;
+  delete temp;
 }
 
 int main() {
-    SL ob;
-    int ch;
-    while (true) {
-        cout << endl << "1. Create \n2. Insertion at beginning \n3. Insertion in between \n4. Insertion at end" << endl;
-        cout << "5. Deletion at beginning \n6. Deletion in between \n7. Deletion at end \n8. Search \n9. Traversal \n10. Exit" << endl;
-        cout << endl << "Enter your choice: ";
-        cin >> ch;
-        switch(ch) {
-            case 1: ob.create(); break;
-            case 2: ob.in_beg(); break;
-            case 3: ob.in_spec(); break;
-            case 4: ob.in_end(); break;
-            case 5: ob.del_beg(); break;
-            case 6: ob.del_spec(); break;
-            case 7: ob.del_end(); break;
-            case 8: ob.search(); break;
-            case 9: ob.traversal(); break;
-            case 10: return 0;
-            default: cout << "Wrong choice";
-        }
+  SLL list;
+  int choice;
+  while(true) {
+    cout << endl << "0. Exit \n1. Create \n2. Traverse \n3. Search \n4. Insert at beg \n5. Insert at end \n6. Insert at pos \n7. Delete at beg \n8. Delete at end \n9. Delete at pos \nEnter your choice: ";
+    cin >> choice;
+    switch(choice) {
+      case 0: return 0;
+      case 1: list.create(); break;
+      case 2: list.traverse(); break;
+      case 3: list.search(); break;
+      case 4: list.insertBeg(); break;
+      case 5: list.insertEnd(); break;
+      case 6: list.insertPos(); break;
+      case 7: list.delBeg(); break;
+      case 8: list.delEnd(); break;
+      case 9: list.delPos(); break;
+      default: cout << "Wrong choice";
     }
-    return 0;
+  }
+  return 0;
 }
