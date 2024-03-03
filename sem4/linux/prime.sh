43c7f63d85a96399693b3ed54407d79b44a342f8
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-03-03 17:16:53 +0530

    fix: fix prime number finder

--- sem4/linux/prime.sh
+++ sem4/linux/prime.sh
@@ -9,12 +9,12 @@ fi
 flag=0
 i=2
 
-while [ $i -le $((n/2)) ]; do
-  if [ $((n % i)) -eq 0 ]; then
+while [ $i -le $(($n / 2)) ]; do
+  if [ $(($n % $i)) -eq 0 ]; then
     flag=1
     break
   fi
-  i=$((i+1))
+  i=$(($i+1))
 done
 
 if [ $flag -eq 0 ]; then
